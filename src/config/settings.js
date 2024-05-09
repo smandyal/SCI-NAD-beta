2c11e1c55cf4e9c01c8485b86d1adb7ea60e84ba
h5371h <htsd18@gmail.com>
2024-05-08 21:27:00 -0700

    :root set to darker colors, native dark theme

--- src/config/settings.js
+++ src/config/settings.js
@@ -3,5 +3,5 @@ export default {
 	description: `Explore how SCI-NAD’s 3D visualization tools are transforming neuroscience research.`,
 	url: `public/social.png`, // No trailing slash!
 	name: `SCI-NAD`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-  enableThemeSwitcher: true,
+  enableThemeSwitcher: false,
 };
