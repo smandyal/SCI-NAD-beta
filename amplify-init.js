e3466a6ecf958ab84c82dc67c9837c4ab6ed3612
h5371h <htsd18@gmail.com>
2024-09-11 03:02:00 -0700

    removed amplify

--- amplify-init.js
+++
@@ -1,8 +0,0 @@
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';
-
-if (awsconfig) {
-  Amplify.configure(awsconfig);
-} else {
-  console.error('aws-exports.js is not properly configured.');
-}
