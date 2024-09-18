ee44ddb007d7d1977a71e440e8041fa1de5afde1
h5371h <htsd18@gmail.com>
2024-09-17 20:52:00 -0700

    vercel web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import lit from "@astrojs/lit";
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   site: 'https://www.sci-nad.com', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true, // Generate sitemap (set to "false" to disable)
   integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
