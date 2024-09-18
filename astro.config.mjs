cb15e2388001198b3e1fdd9a636cd3bc75b71251
h5371h <htsd18@gmail.com>
2024-09-17 20:57:11 -0700

    fix vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,15 @@ import icon from "astro-icon";
 import lit from "@astrojs/lit";
 import vercel from '@astrojs/vercel/serverless';
 
+// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
   site: 'https://www.sci-nad.com', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true, // Generate sitemap (set to "false" to disable)
   integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
+  output: 'hybrid', // Use hybrid mode to maintain static generation while enabling server-side features
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+    // Only apply serverless functions to pages that need them
+    functionPerRoute: true,
+  }),
 });
